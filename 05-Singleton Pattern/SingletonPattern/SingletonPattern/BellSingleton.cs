b1754c3c7b21aced6171b9c6b5c4058cee9fcfc1
KostadinovK <kostadinovk2001@gmail.com>
2019-04-18 19:02:51 +0300

    Implemented Singleton Pattern

---
+++ 05-Singleton Pattern/SingletonPattern/SingletonPattern/BellSingleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SingletonPattern
+{
+    public sealed class BellSingleton
+    {
+        private static BellSingleton instance;
+
+        private static object syncRoot = new Object();
+
+        /*The getter is thread safe*/
+        public static BellSingleton Instance
+        {
+            get 
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                    {
+                        instance = new BellSingleton();
+                    }
+
+                    return instance;
+                }
+            }
+        }
+
+        private BellSingleton() { }
+
+        public void Ring()
+        {
+            Console.WriteLine("Bell is Ringing...");
+        }
+    }
+}
---
+++ 05-Singleton Pattern/SingletonPattern/SingletonPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SingletonPattern
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            BellSingleton bell = BellSingleton.Instance;
+
+            bell.Ring();
+        }
+    }
+}
